74dacfe5dfe39abcaf6422ed2fb7005559770292
RamilShagiev <shagiev.ramil1@gmail.com>
2023-02-04 02:57:15 +0300

    Initial commit

---
+++ ASPNETCoreDataArt/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreDataArt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+
+        public DataController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
